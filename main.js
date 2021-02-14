fba10e93b5b1d7f20e8dcc15cc90c6968d67b81d
wlm.morant <wlm.morant@gmail.com>
2021-02-13 23:33:34 -0500

    firestore

--- main.js
+++ main.js
@@ -66,3 +66,47 @@ $logout.addEventListener('click',(e)=> {
         console.log('signout ok')
     })
 })
+
+//POSTS
+const postsList = document.querySelector('.posts')
+//funcion setupPosts
+const setupPosts = data => {
+    if (data.length){
+        let html = ''
+        data.forEach(doc => {
+            const post = doc.data()
+            console.log(post)
+            const li = `
+            <li class="list-group-item list-group-item-action">
+              <h5>${post.titulo}</h5>
+              <p>${post.descripcion}</p>
+            </li>
+            `
+            html += li
+
+        })
+        postsList.innerHTML = html
+    } else {
+        postsList.innerHTML = `
+        <p class="text-center">Login to see posts</p>
+        `
+    }
+}
+
+//EVENTS
+// listar los datos para los usuarios que esten autenticados
+auth.onAuthStateChanged(user => {
+    if (user){
+        //console.log('sign in fs')
+        fs.collection('posts')
+            .get()
+            .then((snapshot) => {
+                //console.log(snapshot.docs)
+                setupPosts(snapshot.docs)
+            })
+    } else {
+        //console.log('sign out fs')
+        setupPosts([])
+
+    } 
+})
